18e58f3f6edb41885bf80ee72fd4c60b38402b6f
akmal-solakhiddinov <akmalsalokhiddinov03@gmail.com>
2022-10-04 15:33:28 +0500

    df

--- contactForm.php
+++ contactForm.php
@@ -10,5 +10,5 @@ if(isset($_POST['submit'])) {
     $txt = "yuo have recieved an e-mail from ".$name.".\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.html?mailSEnd");
+    header("Location: index.php?mailSend");
 }
