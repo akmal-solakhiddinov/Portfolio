4161c600d0977e0ad75144416b8d45be1f147e7a
akmal-solakhiddinov <akmalsalokhiddinov03@gmail.com>
2022-10-14 13:55:10 +0500

    BHJ

--- js/main.js
+++ js/main.js
@@ -3,39 +3,32 @@ const mode = document.querySelector("#night");
 const body = document.querySelector("body");
 const header = document.querySelector("header");
 let initail = document.querySelector(".text-inform");
-let anchar = document.getElementsByClassName(".link-tag");
-
 
 mode.addEventListener("change", () => {
   body.classList.toggle("nightMode");
   header.classList.toggle(".active");
   initail.classList.toggle(".active");
-
 });
 
 
-document.getElementById("#night").onclick = () => {
-  anchar.classList.toggle(".active")
-};
 
-document.querySelector("#night").onclick = () => {
-};
 
 // Ending of dark mode
 
-let menuBtn = document.querySelector('.global');
+let menuBTN = document.querySelector('.global');
 
 document.querySelector('#menuBtn').onclick = () => {
-    menuBtn.classList.toggle('.active');
-}
-
-
-/* auto */
-
-
-
-/* end///// */
-
+    menuBTN.classList.toggle('.toggle');
+    // readMore.classList.remove('.active');
+    
+  }
+  /* auto */
+  
+  /* end///// */
+  window.onscroll = () => {
+    menuBTN.classList.remove('.toggle');
+  }
+  
 
 /* 
 // fade 
@@ -53,8 +46,51 @@ function reveal() {
        reveals[i].classList.remove("active");
      }
    }
- }
+  }
  
  window.addEventListener("scroll", reveal);
 //  //////////////////////////////////////////
  */
+
+// Counter //////////////////////////////////
+function trimTrailingSlash(string) {
+  if (string != null) {
+    return string.replace(/\/+$/, "");
+  } else {
+    return string;
+  }
+}
+if (!String.prototype.trim) {
+  String.prototype.trim = function () {
+    return this.replace(/^\s+|\s+$/g, "");
+  };
+}
+ctrHref = trimTrailingSlash("https://bachelorthesiswritingservice.com/".trim());
+ctrHref2 = trimTrailingSlash(
+  "https://bachelorthesiswritingservice.com/".trim()
+);
+eInDoc = function (e) {
+  while ((e = e.parentNode)) {
+    if (e == document) {
+      return true;
+    }
+  }
+  return false;
+};
+lCheck = function (l) {
+  if (
+    null != l &&
+    null != l.getAttribute("href") &&
+    (ctrHref === "" ||
+      trimTrailingSlash(l.getAttribute("href").trim()) == ctrHref ||
+      trimTrailingSlash(l.href.trim()) == ctrHref ||
+      trimTrailingSlash(l.getAttribute("href").trim()) == ctrHref2 ||
+      trimTrailingSlash(l.href.trim()) == ctrHref2)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+// /////////////////////////////////////////
+
